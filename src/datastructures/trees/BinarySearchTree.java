bcefc1e109b7c6383babe8fc2475dfa0c124a674
Sergei Mikevich <mikesdepartment@gmail.com>
2023-10-06 12:07:02 +0200

    add BFS and DFS methods for graph traversal

--- src/datastructures/trees/BinarySearchTree.java
+++ src/datastructures/trees/BinarySearchTree.java
@@ -1,5 +1,10 @@
 package datastructures.trees;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Queue;
+import java.util.concurrent.LinkedBlockingQueue;
+
 // TODO: implement remove function
 // TODO: // TODO: write traverse function using recursion
 public class BinarySearchTree {
@@ -60,6 +65,74 @@ public class BinarySearchTree {
 
     }
 
+    public List<Integer> breadthFirstSearch() {
+        Queue<Node> queue = new LinkedBlockingQueue<>();
+        List<Integer> bfsPresentation = new ArrayList<>();
+        if (root == null) {
+            return bfsPresentation;
+        }
+        queue.add(root);
+        while (queue.size() > 0) {
+            Node node = queue.poll();
+            if (node.getLeft() != null) {
+                queue.add(node.getLeft());
+            }
+            if (node.getRight() != null) {
+                queue.add(node.getRight());
+            }
+            bfsPresentation.add(node.getValue());
+        }
+        return bfsPresentation;
+    }
+
+    public List<Integer> breadthFirstSearchRecursive(Queue<Node> queue, List<Integer> bfsPresentation) {
+        if (queue.size() == 0) {
+            return bfsPresentation;
+        }
+        Node node = queue.poll();
+        bfsPresentation.add(node.getValue());
+        if (node.getLeft() != null) {
+            queue.add(node.getLeft());
+        }
+        if (node.getRight() != null) {
+            queue.add(node.getRight());
+        }
+        return breadthFirstSearchRecursive(queue, bfsPresentation);
+    }
+
+    public List<Integer> depthFirstSearchInOrder(Node node, List<Integer> dfsPresentation) {
+        if (node.getLeft() != null) {
+            depthFirstSearchInOrder(node.getLeft(), dfsPresentation);
+        }
+        dfsPresentation.add(node.getValue());
+        if (node.getRight() != null) {
+            depthFirstSearchInOrder(node.getRight(), dfsPresentation);
+        }
+        return dfsPresentation;
+    }
+
+    public List<Integer> depthFirstSearchPreOrder(Node node, List<Integer> dfsPresentation) {
+        dfsPresentation.add(node.getValue());
+        if (node.getLeft() != null) {
+            depthFirstSearchPreOrder(node.getLeft(), dfsPresentation);
+        }
+        if (node.getRight() != null) {
+            depthFirstSearchPreOrder(node.getRight(), dfsPresentation);
+        }
+        return dfsPresentation;
+    }
+
+    public List<Integer> depthFirstSearchPostOrder(Node node, List<Integer> dfsPresentation) {
+        if (node.getLeft() != null) {
+            depthFirstSearchPostOrder(node.getLeft(), dfsPresentation);
+        }
+        if (node.getRight() != null) {
+            depthFirstSearchPostOrder(node.getRight(), dfsPresentation);
+        }
+        dfsPresentation.add(node.getValue());
+        return dfsPresentation;
+    }
+
     public String toString() {
         return root.toString();
     }
@@ -88,6 +161,13 @@ public class BinarySearchTree {
         System.out.println("lookup 200 -> " + tree.lookup(200));
 
         System.out.println("Tree -> " + tree);
+        System.out.println("BFS presentation -> " + tree.breadthFirstSearch());
+        Queue<Node> queue = new LinkedBlockingQueue<>();
+        queue.add(tree.root);
+        System.out.println("BFS presentation recursive -> " + tree.breadthFirstSearchRecursive(queue, new ArrayList<>()));
+        System.out.println("DFS Inorder -> " + tree.depthFirstSearchInOrder(tree.root, new ArrayList<>()));
+        System.out.println("DFS Preorder -> " + tree.depthFirstSearchPreOrder(tree.root, new ArrayList<>()));
+        System.out.println("DFS Postorder -> " + tree.depthFirstSearchPostOrder(tree.root, new ArrayList<>()));
     }
 
 }
