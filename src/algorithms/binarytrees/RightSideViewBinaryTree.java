4760b58c14350ded32761bbd2b20b970879dbd5a
Sergei Mikevich <mikesdepartment@gmail.com>
2023-10-20 11:30:42 +0200

    add one more implementation regarding right side view - DFS for Binary Tree

--- src/algorithms/binarytrees/RightSideViewBinaryTree.java
+++ src/algorithms/binarytrees/RightSideViewBinaryTree.java
@@ -13,7 +13,7 @@ public class RightSideViewBinaryTree {
     //        7
     //       8
     // result -> 1, 3, 6, 7, 8
-    public List<Integer> rightSideView(TreeNode root) {
+    public List<Integer> rightSideViewBFS(TreeNode root) {
         List<Integer> result = new ArrayList<>();
         if (root == null) {
             return result;
@@ -39,12 +39,46 @@ public class RightSideViewBinaryTree {
         return result;
     }
 
+    //             1
+    //         2        3
+    //      4     5    6
+    //        7
+    //       8
+    // result -> 1, 3, 6, 7, 8
+    public List<Integer> rightSideViewDFS(TreeNode root) {
+        List<Integer> result = new ArrayList<>();
+        if (root == null) {
+            return result;
+        } else {
+            rightSideViewDFS1(root, result, 0);
+        }
+        return result;
+    }
+
+    public void rightSideViewDFS1(TreeNode node, List<Integer> result, int currentLevel) {
+        currentLevel++;
+        if (result.size() < currentLevel) {
+            result.add(node.val);
+        }
+        if (node.right != null) {
+            rightSideViewDFS1(node.right, result, currentLevel);
+        }
+        if (node.left != null) {
+            rightSideViewDFS1(node.left, result, currentLevel);
+        }
+    }
+
     public static void main(String[] args) {
         RightSideViewBinaryTree rightSideViewBinaryTree = new RightSideViewBinaryTree();
         TreeNode treeNode = new TreeNode(1, new TreeNode(2, new TreeNode(4, null, new TreeNode(7, new TreeNode(8), null)), new TreeNode(5)), new TreeNode(3, new TreeNode(6), null));
-        System.out.println(rightSideViewBinaryTree.rightSideView(treeNode)); // [1, 3, 6, 7, 8]
-        System.out.println(rightSideViewBinaryTree.rightSideView(new TreeNode(1))); // [1]
-        System.out.println(rightSideViewBinaryTree.rightSideView(null)); // []
+        System.out.println("BFS implementation");
+        System.out.println(rightSideViewBinaryTree.rightSideViewBFS(treeNode)); // [1, 3, 6, 7, 8]
+        System.out.println(rightSideViewBinaryTree.rightSideViewBFS(new TreeNode(1))); // [1]
+        System.out.println(rightSideViewBinaryTree.rightSideViewBFS(null)); // []
+        System.out.println("DFS implementation");
+        System.out.println(rightSideViewBinaryTree.rightSideViewDFS(treeNode)); // [1, 3, 6, 7, 8]
+        System.out.println(rightSideViewBinaryTree.rightSideViewDFS(new TreeNode(1))); // [1]
+        System.out.println(rightSideViewBinaryTree.rightSideViewDFS(null)); // []
     }
 
     public static class TreeNode {
