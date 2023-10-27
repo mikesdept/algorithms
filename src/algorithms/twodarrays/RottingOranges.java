d04c1fd8111a41b0f3ebe9d9e480217b0674eac7
Sergei Mikevich <mikesdepartment@gmail.com>
2023-10-28 00:04:19 +0200

    code improvements regarding rotting oranges algorithm

--- src/algorithms/twodarrays/RottingOranges.java
+++ src/algorithms/twodarrays/RottingOranges.java
@@ -1,5 +1,6 @@
 package algorithms.twodarrays;
 
+import java.util.Arrays;
 import java.util.Queue;
 import java.util.concurrent.LinkedBlockingQueue;
 
@@ -21,6 +22,10 @@ public class RottingOranges {
         }
         int rottenQueueSizeLevel = rottenQueue.size();
         int minutes = 0;
+        boolean[][] taken = new boolean[grid.length][grid[0].length];
+        for (int i = 0; i < taken.length; i++) {
+            Arrays.fill(taken[i], false);
+        }
         while (!rottenQueue.isEmpty()) {
             if (rottenQueueSizeLevel == 0) {
                 rottenQueueSizeLevel = rottenQueue.size();
@@ -37,8 +42,9 @@ public class RottingOranges {
             for (int directionsIndex = 0; directionsIndex < directions.length; directionsIndex++) {
                 int indexIShift = i + directions[directionsIndex][0];
                 int indexJShift = j + directions[directionsIndex][1];
-                if (!(indexIShift < 0 || indexIShift >= grid.length || indexJShift < 0 || indexJShift >= grid[0].length || grid[indexIShift][indexJShift] == 0 || grid[indexIShift][indexJShift] == 2)) {
+                if (!(indexIShift < 0 || indexIShift >= grid.length || indexJShift < 0 || indexJShift >= grid[0].length || grid[indexIShift][indexJShift] == 0 || grid[indexIShift][indexJShift] == 2 || taken[indexIShift][indexJShift])) {
                     rottenQueue.add(new Integer[]{indexIShift, indexJShift});
+                    taken[indexIShift][indexJShift] = true;
                 }
             }
         }
@@ -49,10 +55,16 @@ public class RottingOranges {
         }
     }
 
+    // 2 2
+    // 1 1
+    // 0 0
+    // 2 0
     public static void main(String[] args) {
         RottingOranges rottingOranges = new RottingOranges();
         System.out.println(rottingOranges.orangesRotting(new int[][]{{2, 1, 1, 0, 0}, {1, 1, 1, 0, 0}, {0, 1, 1, 1, 1}, {0, 1, 0, 0, 1}}));
         System.out.println(rottingOranges.orangesRotting(new int[][]{{1, 1, 0, 0, 0}, {2, 1, 0, 0, 0}, {0, 0, 0, 1, 2}, {0, 1, 0, 0, 1}}));
+        System.out.println(rottingOranges.orangesRotting(new int[][]{{2, 2}, {1, 1}, {0, 0}, {2, 0}}));
+        System.out.println(rottingOranges.orangesRotting(new int[][]{{}}));
     }
 
 }
