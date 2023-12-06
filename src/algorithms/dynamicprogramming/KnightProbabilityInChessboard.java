55c0cd3ca00062f49b857735918c2b8bf3d43b24
Sergei Mikevich <mikesdepartment@gmail.com>
2023-12-06 11:31:58 +0100

    solve task about knight probability in chessboard using recursive solution

---
+++ src/algorithms/dynamicprogramming/KnightProbabilityInChessboard.java
@@ -0,0 +1,27 @@
+package algorithms.dynamicprogramming;
+
+public class KnightProbabilityInChessboard {
+
+    private int[][] directions = new int[][]{{-2, -1}, {-2, 1}, {-1, 2}, {1, 2}, {2, 1}, {2, -1}, {1, -2}, {-1, -2}};
+
+    // TODO: recheck logic once again why it works like that
+    public double knightProbabilityRecursive(int n, int k, int row, int column) {
+        if (n <= 0 || row < 0 || row >= n || column < 0 || column >= n) {
+            return 0;
+        }
+        if (k == 0) {
+            return 1;
+        }
+        double result = 0;
+        for (int i = 0; i < directions.length; i++) {
+            result = result + (knightProbabilityRecursive(n, k - 1, row + directions[i][0], column + directions[i][1]) / 8);
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        KnightProbabilityInChessboard knightProbabilityInChessboard = new KnightProbabilityInChessboard();
+        System.out.println(knightProbabilityInChessboard.knightProbabilityRecursive(6, 2, 2, 2));
+    }
+
+}
