b11620c8d9453207395c08cfd8da62c70d9fd2bb
Sergei Mikevich <mikesdepartment@gmail.com>
2024-02-16 21:34:54 +0100

    repeat task from leetcode - sqrt x 69

--- src/leetcode/easy/SqrtX69.java
+++ src/leetcode/easy/SqrtX69.java
@@ -20,11 +20,35 @@ public class SqrtX69 {
         return (int) latestCandidate;
     }
 
+    public int r_mySqrt(int x) {
+        long left = 0;
+        long right = x;
+        long valueRoundedDown = 0;
+        while (left <= right) {
+            long value = (left + right) / 2;
+            if (value * value > x) {
+                right = value - 1;
+            } else if (value * value < x) {
+                valueRoundedDown = value;
+                left = value + 1;
+            } else {
+                return (int) value;
+            }
+        }
+        return (int) valueRoundedDown;
+    }
+
     public static void main(String[] args) {
         SqrtX69 sqrtX69 = new SqrtX69();
         System.out.println(sqrtX69.mySqrt(4)); // 2
         System.out.println(sqrtX69.mySqrt(8)); // 2
+        System.out.println(sqrtX69.mySqrt(1)); // 1
         System.out.println(sqrtX69.mySqrt(2147395599)); // 46339
+
+        System.out.println(sqrtX69.r_mySqrt(4)); // 2
+        System.out.println(sqrtX69.r_mySqrt(8)); // 2
+        System.out.println(sqrtX69.r_mySqrt(1)); // 1
+        System.out.println(sqrtX69.r_mySqrt(2147395599)); // 46339
     }
 
 }
