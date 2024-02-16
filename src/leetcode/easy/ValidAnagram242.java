f3225838072ba4eb8c6e5c6db0b09cfc446b2d18
Sergei Mikevich <mikesdepartment@gmail.com>
2024-02-16 21:21:55 +0100

    repeat task from leetcode - valid anagram 242

--- src/leetcode/easy/ValidAnagram242.java
+++ src/leetcode/easy/ValidAnagram242.java
@@ -30,10 +30,35 @@ public class ValidAnagram242 {
         return map.isEmpty();
     }
 
+    public boolean r_isAnagram(String s, String t) {
+        Map<Character, Integer> map = new HashMap<>();
+        for (int i = 0; i < s.length(); i++) {
+            Character character = s.charAt(i);
+            map.put(character, map.getOrDefault(character, 0) + 1);
+        }
+        for (int i = 0; i < t.length(); i++) {
+            Character character = t.charAt(i);
+            if (!map.containsKey(character)) {
+                return false;
+            } else {
+                int count = map.get(character);
+                if (count > 1) {
+                    map.put(character, count - 1);
+                } else {
+                    map.remove(character);
+                }
+            }
+        }
+        return map.isEmpty();
+    }
+
     public static void main(String[] args) {
         ValidAnagram242 validAnagram242 = new ValidAnagram242();
         System.out.println(validAnagram242.isAnagram("anagram", "nagaram")); // true
         System.out.println(validAnagram242.isAnagram("rat", "car")); // false
+
+        System.out.println(validAnagram242.r_isAnagram("anagram", "nagaram")); // true
+        System.out.println(validAnagram242.r_isAnagram("rat", "car")); // false
     }
 
 }
