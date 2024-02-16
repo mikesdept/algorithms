c0ee5804a847dbba7e1b67ff8456988297bd2ee8
Sergei Mikevich <mikesdepartment@gmail.com>
2024-02-16 22:19:34 +0100

    repeat task from leetcode - relative sort array 1122

--- src/leetcode/easy/RelativeSortArray1122.java
+++ src/leetcode/easy/RelativeSortArray1122.java
@@ -3,6 +3,7 @@ package leetcode.easy;
 import java.util.Arrays;
 import java.util.LinkedHashMap;
 import java.util.Map;
+import java.util.TreeMap;
 
 public class RelativeSortArray1122 {
 
@@ -69,10 +70,36 @@ public class RelativeSortArray1122 {
         return rightIndex - leftIndex + 1;
     }
 
+    public int[] r_relativeSortArray(int[] arr1, int[] arr2) {
+        Map<Integer, Integer> map = new TreeMap<>();
+        int[] result = new int[arr1.length];
+        int resultIndex = 0;
+        for (int number : arr1) {
+            map.put(number, map.getOrDefault(number, 0) + 1);
+        }
+        for (int number : arr2) {
+            for (int i = 0; i < map.get(number); i++) {
+                result[resultIndex] = number;
+                resultIndex++;
+            }
+            map.remove(number);
+        }
+        for (int number : map.keySet()) {
+            for (int i = 0; i < map.get(number); i++) {
+                result[resultIndex] = number;
+                resultIndex++;
+            }
+        }
+        return result;
+    }
+
     public static void main(String[] args) {
         RelativeSortArray1122 relativeSortArray1122 = new RelativeSortArray1122();
         System.out.println(Arrays.toString(relativeSortArray1122.relativeSortArray(new int[]{2, 3, 1, 3, 2, 4, 6, 7, 9, 2, 19}, new int[]{2, 1, 4, 3, 9, 6}))); // [2,2,2,1,4,3,3,9,6,7,19]
         System.out.println(Arrays.toString(relativeSortArray1122.relativeSortArray(new int[]{28, 6, 22, 8, 44, 17}, new int[]{22, 28, 8, 6}))); // [22,28,8,6,17,44]
+
+        System.out.println(Arrays.toString(relativeSortArray1122.r_relativeSortArray(new int[]{2, 3, 1, 3, 2, 4, 6, 7, 9, 2, 19}, new int[]{2, 1, 4, 3, 9, 6}))); // [2,2,2,1,4,3,3,9,6,7,19]
+        System.out.println(Arrays.toString(relativeSortArray1122.r_relativeSortArray(new int[]{28, 6, 22, 8, 44, 17}, new int[]{22, 28, 8, 6}))); // [22,28,8,6,17,44]
     }
 
 }
