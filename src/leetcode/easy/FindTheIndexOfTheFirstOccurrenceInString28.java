dbb8c42aff48b23a6cf1d9ee19cacf53c35f5362
Sergei Mikevich <mikesdepartment@gmail.com>
2024-02-21 22:33:48 +0100

    additional approach for task from leetcode - find the index of the first occurrence in a string 28

--- src/leetcode/easy/FindTheIndexOfTheFirstOccurrenceInString28.java
+++ src/leetcode/easy/FindTheIndexOfTheFirstOccurrenceInString28.java
@@ -16,10 +16,54 @@ public class FindTheIndexOfTheFirstOccurrenceInString28 {
         return -1;
     }
 
+    public int strStrUsingPolynomialHashing(String haystack, String needle) {
+        if (needle.length() > haystack.length()) {
+            return -1;
+        }
+
+        int p = 31;
+
+        long[] pow = new long[haystack.length()];
+        pow[0] = 1;
+        for (int i = 1; i < pow.length; i++) {
+            pow[i] = pow[i - 1] * p;
+        }
+
+        long[] haystackHash = new long[haystack.length()];
+        haystackHash[0] = (long) haystack.charAt(0);
+        for (int i = 1; i < haystackHash.length; i++) {
+            haystackHash[i] = haystackHash[i - 1] + (pow[i] * (long) haystack.charAt(i));
+        }
+
+        long[] needleHash = new long[needle.length()];
+        needleHash[0] = (long) needle.charAt(0);
+        for (int i = 1; i < needleHash.length; i++) {
+            needleHash[i] = needleHash[i - 1] + (pow[i] * (long) needle.charAt(i));
+        }
+
+        int index = 0;
+        while (index < haystack.length() - needle.length() + 1) {
+            if (needleHash[needleHash.length - 1] * pow[index] == getHaystackHash(haystackHash, index, index + needle.length() - 1)) {
+                return index;
+            }
+            index++;
+        }
+        return -1;
+    }
+
+    private long getHaystackHash(long[] haystackHash, int left, int right) {
+        return haystackHash[right] - (left == 0 ? 0 : haystackHash[left - 1]);
+    }
+
     public static void main(String[] args) {
         FindTheIndexOfTheFirstOccurrenceInString28 findTheIndexOfTheFirstOccurrenceInString28 = new FindTheIndexOfTheFirstOccurrenceInString28();
         System.out.println(findTheIndexOfTheFirstOccurrenceInString28.strStr("sadbutsad", "sad")); // 0
         System.out.println(findTheIndexOfTheFirstOccurrenceInString28.strStr("leetcode", "leeto")); // -1
+        System.out.println(findTheIndexOfTheFirstOccurrenceInString28.strStr("a", "a")); // 0
+        System.out.println("==========");
+        System.out.println(findTheIndexOfTheFirstOccurrenceInString28.strStrUsingPolynomialHashing("sadbutsad", "sad")); // 0
+        System.out.println(findTheIndexOfTheFirstOccurrenceInString28.strStrUsingPolynomialHashing("leetcode", "leeto")); // -1
+        System.out.println(findTheIndexOfTheFirstOccurrenceInString28.strStrUsingPolynomialHashing("a", "a")); // 0
     }
 
 }
