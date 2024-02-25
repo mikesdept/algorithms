f4179212b4f7a85d7893fe920d9d3478067fba98
Sergei Mikevich <mikesdepartment@gmail.com>
2024-02-25 12:11:56 +0100

    solve task from leetcode - lowest common ancestor of deepest leaves 1123

--- src/leetcode/medium/LowestCommonAncestorOfDeepestLeaves1123.java
+++ src/leetcode/medium/LowestCommonAncestorOfDeepestLeaves1123.java
@@ -1,34 +1,100 @@
 package leetcode.medium;
 
-import java.util.Map;
-import java.util.TreeMap;
+import java.util.ArrayList;
+import java.util.List;
 
 public class LowestCommonAncestorOfDeepestLeaves1123 {
 
-    // TODO: on leetcode this task is not completed maybe because of misunderstanding from requirements
+    private int leftDirection = -1;
+    private int rightDirection = 1;
+
     public TreeNode lcaDeepestLeaves(TreeNode root) {
-        TreeMap<Integer, TreeNode> map = new TreeMap<>();
-        dfs(root, map, 1);
-        return !map.isEmpty() ? map.get(map.lastKey()) : root;
+        int maxLevel = getMaxLevel(root);
+        int[] directions = new int[maxLevel];
+
+        List<int[]> leavesPath = new ArrayList<>();
+        dfs(root, 1, directions, leavesPath);
+
+        int maxDirectionsSize = 0;
+        for (int[] leavePath : leavesPath) {
+            for (int i = 0; i < leavePath.length; i++) {
+                maxDirectionsSize = Math.max(maxDirectionsSize, leavePath.length);
+            }
+        }
+
+        List<int[]> maxLeavesPath = new ArrayList<>();
+        for (int[] leavePath : leavesPath) {
+            if (leavePath.length == maxDirectionsSize) {
+                maxLeavesPath.add(leavePath);
+            }
+        }
+
+        if (maxLeavesPath.size() == 0) {
+            return root;
+        } else if (maxLeavesPath.size() == 1) {
+            return getNodeByPath(root, maxLeavesPath.get(0), maxLeavesPath.get(0).length - 1);
+        } else {
+            int i = 0;
+            while (i < maxDirectionsSize) {
+                boolean elementsEqual = true;
+                int element = maxLeavesPath.get(0)[i];
+                for (int j = 0; j < maxLeavesPath.size(); j++) {
+                    if (maxLeavesPath.get(j)[i] != element) {
+                        elementsEqual = false;
+                        break;
+                    }
+                }
+                if (!elementsEqual) {
+                    break;
+                }
+                i++;
+            }
+            int indexOfCommonPath = i - 1;
+            if (indexOfCommonPath == -1) {
+                return root;
+            } else {
+                return getNodeByPath(root, maxLeavesPath.get(0), indexOfCommonPath);
+            }
+        }
     }
 
-    private int dfs(TreeNode node, Map<Integer, TreeNode> map, int level) {
+    private int getMaxLevel(TreeNode node) {
         if (node == null) {
             return 0;
         } else {
-            int leftCount = dfs(node.left, map, level + 1);
-            int rightCount = dfs(node.right, map, level + 1);
-
-            if (leftCount == 1 && rightCount == 1) {
-                map.put(level, node);
-            } else if (leftCount == 1 && rightCount == 0) {
-                map.put(level + 1, node.left);
-            } else if (leftCount == 0 && rightCount == 1) {
-                map.put(level + 1, node.right);
-            }
-
+            int leftCount = getMaxLevel(node.left);
+            int rightCount = getMaxLevel(node.right);
             return Math.max(leftCount, rightCount) + 1;
         }
+    }
+
+    private int dfs(TreeNode node, int level, int[] directions, List<int[]> leavesPath) {
+        if (node == null) {
+            return 0;
+        } else {
+            directions[level - 1] = leftDirection;
+            int leftCount = dfs(node.left, level + 1, directions, leavesPath);
+            directions[level - 1] = rightDirection;
+            int rightCount = dfs(node.right, level + 1, directions, leavesPath);
+
+            if (leftCount == 0 && rightCount == 0) {
+                int[] directionsToSave = new int[level - 1];
+                System.arraycopy(directions, 0, directionsToSave, 0, level - 1);
+                leavesPath.add(directionsToSave);
+            }
+            return Math.max(leftCount, rightCount) + 1;
+        }
+    }
+
+    private TreeNode getNodeByPath(TreeNode node, int[] directions, int finalIndex) {
+        for (int pathIndex = 0; pathIndex <= finalIndex; pathIndex++) {
+            if (directions[pathIndex] == leftDirection) {
+                node = node.left;
+            } else {
+                node = node.right;
+            }
+        }
+        return node;
     }
 
     public static void main(String[] args) {
@@ -36,9 +102,11 @@ public class LowestCommonAncestorOfDeepestLeaves1123 {
         TreeNode node1 = new TreeNode(3, new TreeNode(5, new TreeNode(6), new TreeNode(2, new TreeNode(7), new TreeNode(4))), new TreeNode(1, new TreeNode(0), new TreeNode(8)));
         TreeNode node2 = new TreeNode(1);
         TreeNode node3 = new TreeNode(0, new TreeNode(1, null, new TreeNode(2)), new TreeNode(3));
+        TreeNode node4 = new TreeNode(1, new TreeNode(2, new TreeNode(3, null, new TreeNode(6)), new TreeNode(4, null, new TreeNode(5))), null);
         System.out.println(lowestCommonAncestorOfDeepestLeaves1123.lcaDeepestLeaves(node1).val); // 2
         System.out.println(lowestCommonAncestorOfDeepestLeaves1123.lcaDeepestLeaves(node2).val); // 1
         System.out.println(lowestCommonAncestorOfDeepestLeaves1123.lcaDeepestLeaves(node3).val); // 2
+        System.out.println(lowestCommonAncestorOfDeepestLeaves1123.lcaDeepestLeaves(node4).val); // 2
     }
 
     public static class TreeNode {
