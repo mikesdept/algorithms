bc29b7de799d7a3b3b55bcfcfde25015d8346b0c
Sergei Mikevich <mikesdepartment@gmail.com>
2024-04-30 11:47:14 +0300

    optimal solution for task from leetcode - palindrome linked list 234

--- src/leetcode/easy/PalindromeLinkedList234.java
+++ src/leetcode/easy/PalindromeLinkedList234.java
@@ -20,6 +20,48 @@ public class PalindromeLinkedList234 {
         return true;
     }
 
+    public boolean isPalindromeOptimalSolution(ListNode head) {
+        if (head == null) {
+            return true;
+        }
+
+        ListNode halfNode = getHalf(head);
+        ListNode halfReversedNode = reverse(halfNode.next);
+
+        ListNode startNode = head;
+
+        while (halfReversedNode != null) {
+            if (startNode.val != halfReversedNode.val) {
+                return false;
+            }
+            startNode = startNode.next;
+            halfReversedNode = halfReversedNode.next;
+        }
+        return true;
+    }
+
+    private ListNode getHalf(ListNode node) {
+        ListNode slow = node;
+        ListNode fast = node;
+        while (fast.next != null && fast.next.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return slow;
+    }
+
+    private ListNode reverse(ListNode node) {
+        ListNode temp;
+        ListNode next = null;
+        while (node != null) {
+            temp = node.next;
+            node.next = next;
+            next = node;
+            node = temp;
+        }
+        return next;
+    }
+
     public static void main(String[] args) {
         ListNode listNode1 = new ListNode(1, new ListNode(2, new ListNode(2, new ListNode(1))));
         ListNode listNode2 = new ListNode(1, new ListNode(2));
@@ -27,6 +69,8 @@ public class PalindromeLinkedList234 {
         PalindromeLinkedList234 palindromeLinkedList234 = new PalindromeLinkedList234();
         System.out.println(palindromeLinkedList234.isPalindrome(listNode1)); // true
         System.out.println(palindromeLinkedList234.isPalindrome(listNode2)); // false
+        System.out.println(palindromeLinkedList234.isPalindromeOptimalSolution(listNode1)); // true
+        System.out.println(palindromeLinkedList234.isPalindromeOptimalSolution(listNode2)); // false
     }
 
     private static class ListNode {
