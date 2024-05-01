76fd95b05c773d8495a2ba3cd32909b8c2dae666
Sergei Mikevich <mikesdepartment@gmail.com>
2024-05-01 14:03:32 +0300

    optimized solution from leetcode - search in rotated sorted array 33

--- src/leetcode/medium/SearchInRotatedSortedArray33.java
+++ src/leetcode/medium/SearchInRotatedSortedArray33.java
@@ -3,47 +3,45 @@ package leetcode.medium;
 public class SearchInRotatedSortedArray33 {
 
     public int search(int[] nums, int target) {
-        int left = 0;
-        int right = nums.length - 1;
-        if (nums[left] > nums[right]) {
-            while (left < right) {
-                int middle = (left + right) / 2;
-                if (middle > 0 && nums[middle - 1] > nums[middle]) {
-                    left = middle;
-                    break;
-                } else if (nums[right] < nums[middle]) {
-                    left = middle + 1;
-                } else {
-                    right = middle - 1;
-                }
-            }
-
-            if (target == nums[left]) {
-                return left;
-            }
-
-            if (target > nums[0]) {
-                right = left - 1;
-                left = 0;
-            } else if (target < nums[0]) {
-                right = nums.length - 1;
-            } else {
-                return 0;
-            }
+        if (nums[0] <= nums[nums.length - 1]) {
+            return binarySearch(nums, target, 0, nums.length - 1);
         }
+        int minIndex = findMinimum(nums, 0, nums.length - 1);
+        if (target >= nums[minIndex] && target <= nums[nums.length - 1]) {
+            return binarySearch(nums, target, minIndex, nums.length - 1);
+        } else {
+            return binarySearch(nums, target, 0, minIndex - 1);
+        }
+    }
 
-        while (left < right) {
+    private int binarySearch(int[] nums, int target, int left, int right) {
+        while (left <= right) {
             int middle = (left + right) / 2;
-            if (target > nums[middle]) {
-                left = middle + 1;
-            } else if (target < nums[middle]) {
-                right = middle - 1;
-            } else {
+            if (nums[middle] == target) {
                 return middle;
+            } else if (nums[middle] < target) {
+                left = middle + 1;
+            } else {
+                right = middle - 1;
             }
         }
+        return -1;
+    }
 
-        return nums[left] == target ? left : -1;
+    private int findMinimum(int[] nums, int left, int right) {
+        while (left <= right) {
+            int middle = (left + right) / 2;
+            if (nums[middle] > nums[middle + 1]) {
+                return middle + 1;
+            } else if (nums[middle] < nums[middle - 1]) {
+                return middle;
+            } else if (nums[middle] > nums[left]) {
+                left = middle;
+            } else {
+                right = middle;
+            }
+        }
+        return 0;
     }
 
     public static void main(String[] args) {
