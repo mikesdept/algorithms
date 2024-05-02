a3965d7b6fa14f439fb4af00e69a22a62e3d07e7
Sergei Mikevich <mikesdepartment@gmail.com>
2024-05-02 13:30:49 +0300

    optimized solution for task from leetcode - happy number 202

--- src/leetcode/easy/HappyNumber202.java
+++ src/leetcode/easy/HappyNumber202.java
@@ -1,32 +1,25 @@
 package leetcode.easy;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class HappyNumber202 {
 
     public boolean isHappy(int n) {
-        List<Integer> resultList = new ArrayList<>();
-        int result = n;
-        while (result != 1 && !resultList.contains(result)) {
-            resultList.add(result);
-            result = getNumber(result);
-        }
-        return result == 1;
+        int slow = n;
+        int fast = n;
+        do {
+            slow = getSquareNumber(slow);
+            fast = getSquareNumber(getSquareNumber(fast));
+        } while (slow != fast);
+        return slow == 1;
     }
 
-    private int getNumber(int n) {
-        int result = 0;
-        int digit = 0;
-        int pow = -1;
-        int length = String.valueOf(n).length();
-        while (length != 0) {
-            pow++;
-            digit = n / (int) Math.pow(10, pow) % 10;
-            result += digit * digit;
-            length--;
+    private int getSquareNumber(int n) {
+        int answer = 0;
+        while (n > 0) {
+            int remainder = n % 10;
+            answer += remainder * remainder;
+            n /= 10;
         }
-        return result;
+        return answer;
     }
 
     public static void main(String[] args) {
